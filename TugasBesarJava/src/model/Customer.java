8263649dcddee2ba11a7e43bc143cd2776731362
wasupam <80619867+pamoenqkas@users.noreply.github.com>
2022-05-29 18:29:20 +0700

    set get Customer Class

--- TugasBesarJava/src/model/Customer.java
+++ TugasBesarJava/src/model/Customer.java
@@ -11,6 +11,40 @@ public class Customer {
         this.alamat = alamat;
         this.noHP = noHP;
     }
+    
+    public int getId(){
+        return id;
+    }
+    
+    public void setId(int id){
+        this.id = id;
+    }
+    
+    public String getNama(){
+        return nama;
+    }
+    
+    public void setNama(String nama){
+        this.nama = nama;
+    }
+    
+    public String getAlamat(){
+        return alamat;
+    }
+    
+    public void setAlamat(String alamat){
+        this.alamat = alamat;
+    }
+    
+    public String getNoHP(){
+        return noHP;
+    }
+    
+    public void setNoHP(String noHP){
+        this.noHP = noHP;
+    }
+    
+    
 
     // BELUM ADA METHOD
 }
