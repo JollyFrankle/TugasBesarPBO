81e45ac769193ee64c0aef82b69fb1ec2caf1213
Satyo Gusti Anugrah <74964013+sagurah@users.noreply.github.com>
2022-05-29 18:33:52 +0700

    Update Pegawai.java

--- TugasBesarJava/src/model/Pegawai.java
+++ TugasBesarJava/src/model/Pegawai.java
@@ -4,64 +4,66 @@ import java.net.SocketTimeoutException;
 
 public class Pegawai {
     private String namaPegawai;
-    private String idPegawai;
+    private String id;
     private String tglLahir;
-    private String noHandphonePegawai;
+    private String noHP;
     private String jobDesc;
 
-    public Pegawai(String namaPegawai, String idPegawai, String tglLahir, String noHandphonePegawai, String jobDesc){
+    public String getNamaPegawai() {
+        return this.namaPegawai;
+    }
+
+    public void setNamaPegawai(String namaPegawai) {
         this.namaPegawai = namaPegawai;
-        this.idPegawai = idPegawai;
+    }
+
+    public String getId() {
+        return this.id;
+    }
+
+    public void setId(String id) {
+        this.id = id;
+    }
+
+    public String getTglLahir() {
+        return this.tglLahir;
+    }
+
+    public void setTglLahir(String tglLahir) {
         this.tglLahir = tglLahir;
-        this.noHandphonePegawai = noHandphonePegawai;
+    }
+
+    public String getNoHP() {
+        return this.noHP;
+    }
+
+    public void setNoHP(String noHP) {
+        this.noHP = noHP;
+    }
+
+    public String getJobDesc() {
+        return this.jobDesc;
+    }
+
+    public void setJobDesc(String jobDesc) {
+        this.jobDesc = jobDesc;
+    }
+
+    public Pegawai(String namaPegawai, String id, String tglLahir, String noHP, String jobDesc){
+        this.namaPegawai = namaPegawai;
+        this.id = id;
+        this.tglLahir = tglLahir;
+        this.noHP = noHP;
         this.jobDesc = jobDesc;
     }
     
-    public String getNama(){
-        return namaPegawai;
-    }
     
-    public void setNama(String namaPegawai){
-        this.namaPegawai = namaPegawai;
-    }
-    
-    public String setidPegawai(){
-        return idPegawai;
-    }
-    
-    public void setidPegawai(String idPegawai){
-        this.idPegawai = idPegawai;
-    }
-    
-    public String gettglLahir(){
-        return tglLahir;
-    }
-    
-    public void settglLahir(String tglLahir){
-        this.tglLahir = tglLahir;
-    }
-    
-    public String getnoHandphonePegawai(){
-        return noHandphonePegawai;
-    }
-    
-    public void setnoHandphonePegawai(String noHandphonePegawai){
-        this.noHandphonePegawai = noHandphonePegawai;
-    }
-    
-    public String getjobDesc(){
-        return jobDesc;
-    }
-    
-    public void setjobDesc(String jobDesc){
-        this.jobDesc = jobDesc;
-    }
 
     public void showDataPegawai(){
         System.out.println("Nama Pegawai: "+namaPegawai);
-        System.out.println("ID Pegawai: "+idPegawai);
+        System.out.println("ID Pegawai: "+id);
         System.out.println("Tanggal Lahir: "+tglLahir);
-        System.out.println("No Handphone: "+noHandphonePegawai);
+        System.out.println("No Handphone: "+noHP);
         System.out.println("Job Desc: "+jobDesc);
     }
 
