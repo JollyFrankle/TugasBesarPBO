6bcba1f639dfa227991386a449b59e231e79d834
CalvinAndrean <calvinandrean456@gmail.com>
2022-05-29 17:31:22 +0700

    Added CustomerDBO and TransaksiDBO

---
+++ TugasBesarJava/src/dao/CustomerDBO.java
@@ -0,0 +1,15 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+
+package dao;
+
+/**
+ * Nama : Calvin Andrean Suhedy
+ * NPM : 200710824
+ * Kelas : PBO B
+ */
+public class CustomerDBO {
+
+}
