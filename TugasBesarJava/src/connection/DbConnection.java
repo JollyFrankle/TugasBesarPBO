05ad162f123a3a7e260317f5036eddf777605021
Jolly Frankle <jollyfrankle3@gmail.com>
2022-05-29 14:40:43 +0700

    Create DbConnection.java

---
+++ TugasBesarJava/src/connection/DbConnection.java
@@ -0,0 +1,15 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+
+package connection;
+
+/**
+ * Jolly Hans Frankle
+ * 200710932
+ * Praktikum PBO kelas B
+ */
+public class DbConnection {
+    
+}
