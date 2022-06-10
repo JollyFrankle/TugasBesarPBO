4051bbdf5c02115caf0acddff54474ffa7f9fda2
CalvinAndrean <calvinandrean456@gmail.com>
2022-06-10 15:06:48 +0700

    Create TableCustomer.java
    created TableCustomer

---
+++ TugasBesarJava/src/table/TableCustomer.java
@@ -0,0 +1,62 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+
+package table;
+
+import java.util.List;
+import model.Customer;
+
+/**
+ * Nama : Calvin Andrean Suhedy
+ * NPM : 200710824
+ * Kelas : PBO B
+ */
+public class TableCustomer {
+    List<Customer> list;
+    
+    public TableCustomer(List<Customer> list){
+        this.list = list;
+    }
+    
+    public int getRowCount(){
+        return list.size();
+    }
+    
+    public int getColumnCount(){
+        return 4;
+    }
+    
+    public Object getValueAt(int rowIndex, int columnIndex){
+        switch(columnIndex){
+            case 0:
+                return list.get(rowIndex).getId();
+            case 1:
+                return list.get(rowIndex).getNama();
+            case 2:
+                return list.get(rowIndex).getAlamat();
+            case 3:
+                return list.get(rowIndex).getNoHP();
+            case 99:
+                return list.get(rowIndex);
+            default:
+                return null;
+        }
+    }
+    
+    public String getColumnName(int column){
+        switch(column){
+            case 0:
+                return "ID";
+            case 1:
+                return "Nama";
+            case 2:
+                return "Alamat";
+            case 3:
+                return "Nomor HP";
+            default:
+                return null;           
+        }
+    }
+}
