9cc76497ba2a0da71e4895076b3abfd9704f12f6
I Putu Agestya Pramana <200710994@students.uajy.ac.id>
2022-06-14 17:25:46 +0700

    Control JobHistory

--- TugasBesarJava/src/control/JobHistoryControl.java
+++ TugasBesarJava/src/control/JobHistoryControl.java
@@ -6,7 +6,28 @@ package control;
  */
 
 import dao.JobHistoryPreparedDAO;
+import java.util.List;
+import model.Pegawai;
+import model.JobHistory;
+import table.TabelJobHistory;
 
 public class JobHistoryControl {
+    private JobHistoryPreparedDAO tpDAO = new JobHistoryPreparedDAO();
     
+    public void insertDataJobHistory(JobHistory t){
+        // Insert dibutuhkan juga siapa pegawai yang menerima dan mencatat ini ke sistem.
+        tpDAO.insertJobHistory(t);
+    }
+    
+    public void updateDataJobHistory(JobHistory t){
+        tpDAO.updateJobHistory(t);
+    }
+    
+    public void deleteDataJobHistory(int id){
+        tpDAO.deleteJobHistory(id);
+    }
+    
+    public TabelJobHistory searchJobHistory(int input){
+        return new TabelJobHistory(tpDAO.getJobHistory(input));
+    }
 }
